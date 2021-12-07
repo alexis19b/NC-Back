9fed0301360b8b68e797f0557dc88cab09d142fb
Leandro Buzeta <leandrobuzeta@gmail.com>
2021-12-07 15:58:20 -0300

    otro intento

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use("/api/members", useMember);
 app.use("/api/teams", useTeam);
 app.use("/api/users", useUser);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en el puerto${process.env.PORT}`);
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
